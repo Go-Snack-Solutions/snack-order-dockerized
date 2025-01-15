8e65324fefd8a3af52c6af5c98ca86f02b27df4f
jojocarvalho <joncarvalho@uolinc.com>
2025-01-14 21:31:08 -0300

    creating stop-dockerized bat

--- script-bat/stop-local-snack-order-infrastructures.bat
+++ script-bat/stop-local-snack-order-infrastructures.bat
@@ -5,14 +5,20 @@ echo *--------- STOP FOOD-ORDER INFRASTRUCTURE ----------*
 echo *---------------------------------------------------*
 echo.
 
+echo.
+echo.
 echo docker stop $(docker ps -a -q)
 for /f "tokens=*" %%i in ('docker ps -a -q') do (
     docker stop %%i
 )
 
+echo.
+echo.
 echo docker container prune -f
 docker container prune -f
 
+echo.
+echo.
 echo docker network prune -f
 docker network prune -f
 
